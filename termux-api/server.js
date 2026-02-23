3ab660a2d6fe0a9ce3f5a52416153a70663c6a02
Indrasevendev <indrasevendev@gmail.com>
2026-02-24 02:19:37 +0700

    update

--- termux-api/server.js
+++ termux-api/server.js
@@ -4,25 +4,47 @@ const { exec } = require('child_process');
 const web3 = require('./modules/web3');
 const app = express();
 
-// Rute untuk halaman utama
+// Halaman Utama
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dashboard.html');
 });
 
-// API untuk data Dashboard
+// API Status (Baterai & Blockchain)
 app.get('/api/status', async (req, res) => {
-    const walletData = await web3.getWalletStatus(process.env.MY_WALLET);
-    
-    exec('termux-battery-status', (error, stdout) => {
-        let battery = "N/A";
-        if (!error) {
-            const data = JSON.parse(stdout);
-            battery = data.percentage + "%";
-        }
-        res.json({
-            device: { battery: battery },
-            blockchain: walletData
+    try {
+        const walletData = await web3.getWalletStatus(process.env.MY_WALLET);
+        exec('termux-battery-status', (error, stdout) => {
+            let battery = "N/A";
+            if (!error) {
+                battery = JSON.parse(stdout).percentage + "%";
+            }
+            res.json({
+                device: { battery },
+                blockchain: {
+                    ...walletData,
+                    basename: process.env.MY_BASENAME,
+                    address: process.env.MY_WALLET
+                }
+            });
         });
+    } catch (err) {
+        res.json({ success: false, error: err.message });
+    }
+});
+
+// Command Center: Vibrate
+app.get('/api/vibrate', (req, res) => {
+    exec('termux-vibrate -d 500', (err) => {
+        res.json({ success: !err });
+    });
+});
+
+// Command Center: Torch
+let torchOn = false;
+app.get('/api/torch', (req, res) => {
+    torchOn = !torchOn;
+    exec(`termux-torch ${torchOn ? 'on' : 'off'}`, (err) => {
+        res.json({ success: !err, mode: torchOn });
     });
 });
 
